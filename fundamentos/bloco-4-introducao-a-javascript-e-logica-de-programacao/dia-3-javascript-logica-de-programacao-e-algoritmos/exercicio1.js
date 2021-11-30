61449759c09ccc501fdba825875f5df2d967cdde
Jéssica Florentino <jdgflorentino@gmail.com>
2021-11-30 18:13:20 -0300

    Exercicio 1: quadrado de asteriscos.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -0,0 +1,15 @@
+let n = 9;
+let ast = '*';
+let resultado = '';
+
+
+
+for (let i = 1; i <= n; i += 1) {
+
+    resultado = resultado + ast;
+
+}
+
+for (let i = 1; i <= n; i += 1) {
+    console.log(resultado);
+}
