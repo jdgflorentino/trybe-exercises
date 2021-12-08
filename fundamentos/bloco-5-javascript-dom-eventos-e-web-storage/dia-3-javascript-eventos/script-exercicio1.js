b600714940ed6e2300954d86540ce4956f3eedd7
Jéssica Florentino <jdgflorentino@gmail.com>
2021-12-08 10:14:46 -0300

    Funçao botao feriados.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script-exercicio1.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script-exercicio1.js
@@ -63,3 +63,21 @@ function createDaysOfTheMonth() {
     };
 };
 createDaysOfTheMonth();
+
+// Exercício 2
+
+// Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+// Adicione a este botão a ID "btn-holiday" .
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+
+function holidays(feriados) {
+
+    let button = document.createElement('button');
+    let elementDiv = document.querySelector('.buttons-container');
+    button.innerHTML = feriados;
+    button.id = 'btn-holiday';
+    elementDiv.appendChild(button);
+
+};
+
+holidays('Feriados');
