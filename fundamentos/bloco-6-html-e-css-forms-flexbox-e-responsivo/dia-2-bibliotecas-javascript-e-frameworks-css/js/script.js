22b94dfa35636ebcaaeefe6d5c570115f5826351
Jéssica Florentino <jdgflorentino@gmail.com>
2022-01-06 14:36:59 -0300

    Adicionei biblioteca Pikaday.

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/js/script.js
@@ -0,0 +1,39 @@
+var picker = new Pikaday({
+    field: document.getElementById('datepicker'),
+    format: 'DD/MM/YYYY',
+    toString(date, format) {
+        // you should do formatting based on the passed format,
+        // but we will just return 'D/M/YYYY' for simplicity
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    },
+    parse(dateString, format) {
+        // dateString is the result of `toString` method
+        const parts = dateString.split('/');
+        const day = parseInt(parts[0], 10);
+        const month = parseInt(parts[1], 10) - 1;
+        const year = parseInt(parts[2], 10);
+        return new Date(day, month, year);
+    }
+});
+
+
+
+function createStateOptions() {
+    let states = document.getElementById('state');
+    let stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+
+    for (let index = 0; index < stateOptions.length; index += 1) {
+        let option = document.createElement('option');
+        option.innerText = stateOptions[index];
+        option.value = stateOptions[index];
+        states.appendChild(option);
+    }
+}
+
+window.onload = function() {
+    createStateOptions();
+
+}
